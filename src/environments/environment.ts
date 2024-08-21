5f5dd9dd664e215f253e64d63ddfa956afa4d837
namnhhe161918 <121742978+devnamnh2k2@users.noreply.github.com>
2024-08-21 19:10:57 +0700

    edit /docs/browser/index.html

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    firebaseConfig: {
+        apiKey: "AIzaSyCbkO5VYvg4QyFP-iMwPYurGwQJkqJgTnM",
+        authDomain: "bookingfpt.firebaseapp.com",
+        databaseURL: "https://bookingfpt-default-rtdb.asia-southeast1.firebasedatabase.app",
+        projectId: "bookingfpt",
+        storageBucket: "bookingfpt.appspot.com",
+        messagingSenderId: "854133491171",
+        appId: "1:854133491171:web:ec65999c8fb103a6094cd6",
+        measurementId: "G-D9WVJRT50K"
+    }
+};
